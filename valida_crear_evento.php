87d84ff82b2e2a5b0142d1fee9e9b58b869143f6
rafavermo@gmail.com <rafavermo@gmail.com@59109379-8167-2722-ed6a-bf78f8178829>
2011-12-30 18:19:13 +0000

    valida_crear_evento

--- valida_crear_evento.php
+++ valida_crear_evento.php
@@ -11,29 +11,55 @@
  $errores=$_SESSION["errores"];
  
  if(isset($formularioEvento)){
-  	 //Recogemos todos los datos
+ 	 //hacemos el tratamiento de la fecha
+ 	 $fecha=$_REQUEST["fecha"];
+ 	 $fecha1= explode("/",$fecha);
+	 $dia=$fecha1[0];
+	 $mes=$fecha1[1];
+	 $anyo=$fecha1[2];
+	 $hora=$_REQUEST["hora"];
+	 if(!(strlen($fecha)>7 && strlen($fecha)<11 && checkdate($mes,$dia,$anyo))){
+	 	$errores[]='<font color="red">El campo <b>fecha</b> no cumple el patron determinado DD/MM/YYYY</font><p>';
+	 }else{
+	 	$sfecha=$anyo."-".$mes."-".$dia." ".$hora.":"."00";
+	 }
+	 
   	 
+  	 //Recogemos todos los datos
+  	 $usuario=$_SESSION["usuario"];
+  	 $centroID=$_REQUEST["centroID"];
+	 $pistaID=$_REQUEST["pistaID"];
   	 
   	  //Para recoger el id del usuario actual conectado
   	 $conexion=conectaBASEDATOS();
-	 $usuarioID=seleccionaUsuarioIDporNombre($_SESSION["usuario"], $conexion);
+	 $usuarioID=seleccionaUsuarioIDporNombre($usuario, $conexion);
+	 $evento=consultaExisteEvento($centroID,$pistaID,$sfecha,$conexion);
   	 desconectaBASEDATOS($conexion);
-	 
+  	 
+	 if($evento->rowCount()!=0){
+		$errores[]='<font color="red">Ya existe un <b>evento</b> con las opciones elegidas</font><p>';
+	 }
+	
 	 foreach ($usuarioID as $id){
 	 	  $_SESSION["usuarioID"]=$id["usuarioID"];
 	 }
 	 
 	 //Recogemos los demas datos
- 	$formularioEvento["centroID"]=$_REQUEST["centroID"];
- 	$formularioEvento["pistaID"]=$_REQUEST["pistaID"];
- 	$formularioEvento["fecha"]=$_REQUEST["fecha"]; 
-	$formularioEvento["hora"]=$_REQUEST["hora"];
+ 	$formularioEvento["centroID"]=$centroID;
+ 	$formularioEvento["pistaID"]=$pistaID;
+ 	$formularioEvento["fecha"]=$sfecha; 
+	$formularioEvento["evento"]=$evento->rowCount();
 		 
-		 
-	  $_SESSION["crea_evento"]=$formularioEvento;
-	  //Calculamos los errores del formulario
-	  $errores=valida_crea_evento($formularioEvento); 
-	 
+	$_SESSION["crea_evento"]=$formularioEvento;
+	
+	//Si encontramos errores del formulario
+   if(count($errores) > 0){
+   	 $_SESSION["errores"] = $errores;
+	 Header("Location: index.php");  
+   }else{
+     //Si no hay errores
+   	 Header("Location: exitocrearevento.php");
+   } 
 	
  }else{
  	Header("Location: index.php");
@@ -42,39 +68,6 @@
 	
 	
 	
-	//Si encontramos errores del formulario
-   if(count($errores) > 0){
-   	 $_SESSION["errores"] = $errores;
-	 Header("Location: index.php");  
-    }else{
-      //Si no hay errores
-   	   Header("Location: exitocrearevento.php");
-   } 
-	 
-
- function valida_crea_evento($formulario){
-   
- 
-  list($dia,$mes,$year) = explode("/",$formulario["fecha"]);
-           
-		   // echo "ANTES DE ENTRAR en el if, existe fecha?:". isset($formulario["fecha"]) . "es distindo de vacio?:"  . $formulario["fecha"]!=""; 
-		   
-           if( isset($formulario["fecha"]) && $formulario["fecha"]!=''){
-           	            		
-           	            	
-           	            
-                         if(! (strlen($formulario["fecha"])>7 && strlen($formulario["fecha"])<11 && checkdate($mes,$dia, $year)  ) ) {
-                         	 
-                             $errores[]='<font color="red">El campo <b>fecha</b> no cumple el patron determinado</font><p>';
-							                  
-                          }
-           }else{
-           	  $errores[]='<font color="red">Debe introducir una <b>fecha</b></font><p>';
-           }
-	       
- 
-      return $errores;
- }
-  
-		   
+	
+	    
 ?>
