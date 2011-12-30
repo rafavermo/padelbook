d61a8cc1976c9f1e675925d71e47e481531bd43f
emoronayuso@gmail.com <emoronayuso@gmail.com@59109379-8167-2722-ed6a-bf78f8178829>
2011-12-30 11:47:44 +0000

    Cabecera fina arreglada, se quita el salto de linea del formulario para que el reloj y el boton login esten en la misma linea y dentro del div

--- cabecera_fina.php
+++ cabecera_fina.php
@@ -1,8 +1,11 @@
+<form name="form_reloj" id="form_reloj" style="display:inline;">  <input type="text" name="reloj" id="reloj" size="10" style="background-color : Yellow; color : Black; font-family : Verdana, Arial, Helvetica; font-size : 8pt; text-align : center;" onfocus="window.document.form_reloj.reloj.blur()"> <!-- La funcion blur() es para que no se ponga el foco del raton en el reloj ya que es un input tipo text --> </form> 
+
 <?php
 if(isset($_SESSION["usuario"]) && $_SESSION["usuario"]!="" ){
 	$user = $_SESSION['usuario'];
-   echo "<form action='cerrar_sesion.php' method='post' style='text-align:right'>Hola $user <input type='submit' name='cerrar_sesion' value='cerrar sesion'></form>";
+   echo "<form action='cerrar_sesion.php' method='post' style='float:right; display:inline'>Hola $user <input type='submit' name='cerrar_sesion' value='cerrar sesion'></form>";
   }else{
-  	echo "<form action='menusinjavascript.php' method='post' style='text-align:right'><input type='submit' name='login' value='login'></form>";
+  	echo "<form action='menusinjavascript.php' method='post' style='float:right; display:inline'><input type='submit' name='login' value='login'></form>";
   }
 ?>
+
