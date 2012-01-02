4af898cd22ad4ea1385c34dff9acae772bc2bb40
rafavermo@gmail.com <rafavermo@gmail.com@59109379-8167-2722-ed6a-bf78f8178829>
2012-01-02 18:22:13 +0000

    Modificacion para grupo y perfil

--- cabecera_fina.php
+++ cabecera_fina.php
@@ -1,15 +1,20 @@
 <div id="cabecera_fina">
 	<div id="form_reloj">
-		<form name="form_reloj" id="form_reloj" style="display:inline;">  <input type="text" name="reloj" id="reloj" onfocus="window.document.form_reloj.reloj.blur()"> <!-- La funcion blur() es para que no se ponga el foco del raton en el reloj ya que es un input tipo text --> </form> 
+		<form name="form_reloj" id="form_reloj" style="display:inline;">  <input type="text" name="reloj" id="reloj" onfocus="window.document.form_reloj.reloj.blur()"> <!-- La funcion blur() es para que no se ponga el foco del raton en el reloj ya que es un input tipo text --> </form>
+	</div> 
 		<?php
 		if(isset($_SESSION["usuario"]) && $_SESSION["usuario"]!="" ){
 			$user = $_SESSION['usuario'];
    			// echo "<form action='cerrar_sesion.php' method='post' style='float:right; display:inline'> $user    <a href='' onclick='perfil()'>Perfil&nbsp;</a>/ <a href='index.php?contenido=grupos.php' onclick='submit'>Grupos</a> <input type='submit' name='cerrar_sesion' value='cerrar sesion'></form>";
-   			echo "<div id='menu_inicio' class='opcionactiva0'><a href='cerrar_sesion.php'>Cerrar Sesion</a></div>";
+   			echo "<div id='menu_inicio' class='opcionactiva0'>
+   					<a href='cerrar_sesion.php'>Cerrar Sesion</a></div>
+   					<div id='menu_inicio' class='opcionactiva0'> 
+   					<a href='' onclick='perfil()'>Perfil</a></div> 
+   					<div id='menu_inicio' class='opcionactiva0'>
+   					<a href='index.php?contenido=grupos.php' onclick='submit'>Grupos</a> </div>";
   		}else{
   			// echo "<form action='index.php?contenido=login.html' method='post' style='float:right; display:inline'><input type='submit' name='login' value='login'></form>";
   			echo "<div id='menu_inicio' class='opcionactiva0'><a href='index.php?contenido=login.html'>Login</a></div>";
   		}
 		?>
-	</div>
 </div>
