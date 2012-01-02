780027af791b4947af900abab83951473c0453fb
rafavermo@gmail.com <rafavermo@gmail.com@59109379-8167-2722-ed6a-bf78f8178829>
2012-01-02 17:49:59 +0000

    Reestructuracion de index.php

--- trunk/index.php
+++ trunk/index.php
@@ -30,76 +30,76 @@
 </head>
 
   
-<body onload=mostrarMenuConJavascript();>
-	 
+<body onload=mostrarMenuConJavascript();> 
+	
+	<div id="header">
+		<div id="logotipo">
+		</div>
 		
-	<div id="logotipo">
-	
+		<div id="logo">
+			<h1>Padelbook</h1>
+			<p><a href="">una nueva experiencia</a>
+			</p> 
+		</div>
 	</div>
+		
+	
 
-<div id="header">
-	<h1></h1>
-</div>
+	<!--<div id="header">
+		<h1></h1>
+	</div> -->
  
-<div id="cabecera_fina">
-	 
-  <?php include_once("cabecera_fina.php"); ?>
-
-</div>
-<div id="cuerpo">
-<div id="columna_izquierda">
-  
-<noscript><?php include("menuindexsinjavascript.html");?> </noscript>
-
-<!--div trasnparente que se muestra si esta activado javascript -->
- <div id="divmenuConJavascript" >
- 	<?php include("menuindexconjavascript.html");?>
- </div>	
 	
-</div> <!-- div columna_izquierda -->
-
-<div id="columna_derecha">
-     <ul><li><a href="./reglamento/REGLAMENTO_JUEGO_FIP_2008.pdf">Reglamento FIP 2008</a></li>
-     	 <li><a href="http://www.padelfip.org">Web oficial de la FIP</a></li>
-     	 <li><a href="http://www.padelfederacion.es">Web oficial de la FEP</a></li>
-     	 <li><a href="http://www.bwinpadelprotour.com/">Web Padel Pro Tour</a></li>
-     </ul>
-</div>
-
-<div id="contenido">
+ 	<?php include_once("cabecera_fina.php"); ?>
 	
-<?php
- 
-  
-   //si existen errores por la validacion PHP por parte del servidor en el registro los imprimimos
-	if(isset($errores) && count($errores)>0 && $errores!=""){
-	  
-      foreach($errores as $elemento){
-      	//echo "-----" . count($_SESSION["errores"]);
-      	//for($i=0;$i<=count($_SESSION["errores"]);$i++){
-	  	   printf("$elemento");
-	   }
-    }
-	//Destruimos la variable con los errores
-	unset($_SESSION["errores"]);
 	
-	//Mostramos el mensaje de exito si existe REQUEST[exito] con algun valor determinado
-	if(isset($_REQUEST["exito"]) && $_REQUEST["exito"]!=""){
-		include("exito.php");
-	}
+	<div id="cuerpo">
+		<div id="columna_izquierda">
+  			<noscript><?php include("menuindexsinjavascript.html");?> </noscript>
+				<!--div trasnparente que se muestra si esta activado javascript -->
+ 				<div id="divmenuConJavascript" >
+ 					<?php include("menuindexconjavascript.html");?>
+ 				</div>	
+		</div> <!-- div columna_izquierda -->
 
-   //incluye en el div contenido, el contenido de la pagina especificada por $contenido (variable de sesion)
-   if($contenido!=""){
-     include($contenido);
-   }	   
-?>
-	 
-         
-</div> <!-- div Contenido -->
-</div>
-<div id="pie">
+		<div id="columna_derecha">
+     		<ul>
+     			<li><a href="./reglamento/REGLAMENTO_JUEGO_FIP_2008.pdf">Reglamento FIP 2008</a></li>
+     	 		<li><a href="http://www.padelfip.org">Web oficial de la FIP</a></li>
+     	 		<li><a href="http://www.padelfederacion.es">Web oficial de la FEP</a></li>
+     	 		<li><a href="http://www.bwinpadelprotour.com/">Web Padel Pro Tour</a></li>
+     		</ul>
+		</div>
+
+		<div id="contenido">
+			<?php
+   			//si existen errores por la validacion PHP por parte del servidor en el registro los imprimimos
+			if(isset($errores) && count($errores)>0 && $errores!=""){
+	  			foreach($errores as $elemento){
+      				//echo "-----" . count($_SESSION["errores"]);
+      				//for($i=0;$i<=count($_SESSION["errores"]);$i++){
+	  	   			printf("$elemento");
+	   			}
+    		}
+			//Destruimos la variable con los errores
+			unset($_SESSION["errores"]);
+	
+			//Mostramos el mensaje de exito si existe REQUEST[exito] con algun valor determinado
+			if(isset($_REQUEST["exito"]) && $_REQUEST["exito"]!=""){
+				include("exito.php");
+			}
+
+   			//incluye en el div contenido, el contenido de la pagina especificada por $contenido (variable de sesion)
+   			if($contenido!=""){
+     			include($contenido);
+   			}	   
+			?>
+		</div> <!-- div Contenido -->
+	</div>
+	
+	<div id="pie">
 	Este es el pie 
-</div>
+	</div>
 
 </body>
 </html>
