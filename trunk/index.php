0411891b03f889bd5c77bcbb14a26d49d96fdd1b
rafavermo@gmail.com <rafavermo@gmail.com@59109379-8167-2722-ed6a-bf78f8178829>
2012-01-03 18:35:10 +0000

    Remodelación del index

--- trunk/index.php
+++ trunk/index.php
@@ -31,9 +31,11 @@
 
   
 <body onload=mostrarMenuConJavascript();> 
-	
+<div class="wrapper">
+	<div class="page">
 	<div id="header">
 		<div id="logotipo">
+			
 		</div>
 		
 		<div id="logo">
@@ -41,6 +43,18 @@
 			<p><a href="">una nueva experiencia</a>
 			</p> 
 		</div>
+		<!-- <form id="search_mini_form" method="get" action="http://www.cooking-hacks.com/index.php/catalogsearch/result/">
+			<div class="form-search">
+				<label for="search">Search:</label>
+				<input id="search" class="input-text" type="text" value="" name="q" autocomplete="off">
+				<button class="button" title="Search" type="submit">
+				<span>
+					<span>Search</span>
+				</span>
+				</button>
+				<div id="search_autocomplete" class="search-autocomplete" style="display: none;"></div>
+			</div>
+		</form> -->
 	</div>
 		
 	
@@ -100,6 +114,8 @@
 	<div id="pie">
 	Este es el pie 
 	</div>
+	</div>
+</div>
 
 </body>
 </html>
